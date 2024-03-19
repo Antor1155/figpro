d224ed3f539d4c0e680a302005af1176a57694cc
Md Antor <md.antor1155@gmail.com>
2024-03-19 07:14:02 +0600

    cursor chat functionality added

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,6 +1,6 @@
 import { useMyPresence, useOthers } from "@/liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import CursorChat from "./cursor/CursorChat";
 import { CursorMode } from "@/types/type";
 
@@ -30,6 +30,37 @@ export default function Live() {
         const y = event.clientY - event.currentTarget.getBoundingClientRect().y
 
         updateMyPresence({ cursor: { x, y } })
+    }, [])
+
+    useEffect(()=>{
+        const onKeyUp = (e:KeyboardEvent) =>{
+            if(e.key === '/'){
+                setCursorState({
+                    mode:CursorMode.Chat,
+                    previousMessage: null,
+                    message: "",
+                })
+            } else if(e.key === "Escape"){
+                updateMyPresence({message: ""})
+                setCursorState({mode: CursorMode.Hidden})
+            }
+        }
+
+        const onKeyDown = (e: KeyboardEvent) =>{
+            if(e.key === "/"){
+                e.preventDefault()
+            }
+        }
+
+        window.addEventListener("keyup", onKeyUp)
+        window.addEventListener("keydown", onKeyDown)
+
+
+        return () =>{
+            window.removeEventListener("keyup", onKeyUp)
+            window.removeEventListener("keydown", onKeyDown)
+        }
+
     }, [])
 
     return (
