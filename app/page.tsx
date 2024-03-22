cf5d98290119445efe5d4ac8571dedc0ff2dda43
Md Antor <md.antor1155@gmail.com>
2024-03-22 14:17:26 +0600

    added loader and provided with initialPresence and initialStorage

--- app/page.tsx
+++ app/page.tsx
@@ -9,50 +9,75 @@ import LeftSideBar from "@/components/LeftSideBar";
 import RightSideBar from "@/components/RightSideBar";
 import { useEffect, useRef, useState } from "react";
 import { ActiveElement, CustomFabricObject } from "@/types/type";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { handleCanvasMouseDown, handleCanvaseMouseMove, handleResize, initializeFabric } from "@/lib/canvas";
+import { useMutation, useStorage } from "@/liveblocks.config";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const fabricRef = useRef<fabric.Canvas | null >(null)
+  const fabricRef = useRef<fabric.Canvas | null>(null)
   const isDrawing = useRef(false)
-
   const shapeRef = useRef<fabric.Object | null>(null)
   const selectedShapeRef = useRef<string | null>("rectangle")
 
+  const canvasObjects = useStorage((root) => root.canvasObjects)
+
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if (!object) return;
+
+    const { objectId } = object
+    const shapeData = object.toJSON()
+    shapeData.objectId = objectId
+
+    const canvasObjects = storage.get("canvasObjects")
+
+    canvasObjects.set(objectId, shapeData)
+
+  }, [])
+
   const [activeElement, setActiveElement] = useState<ActiveElement>({
-    name:"",
-    value:"",
-    icon:"",
+    name: "",
+    value: "",
+    icon: "",
   })
 
-  const handleActiveElement = (elem:ActiveElement) => {
+  const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem)
-
     selectedShapeRef.current = elem?.value as string
   }
 
-  useEffect(()=>{
-    const canvas = initializeFabric({canvasRef, fabricRef})
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef })
 
     canvas.on("mouse:down", (options) => {
       handleCanvasMouseDown({
         options,
-        canvas, 
+        canvas,
         isDrawing,
         shapeRef,
         selectedShapeRef
       })
     })
 
-    window.addEventListener("resize", () => {
-      handleResize({fabricRef})
+    canvas.on("mouse:move", (options) => {
+      handleCanvaseMouseMove({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage
+      })
     })
 
-  },[])
+    window.addEventListener("resize", () => {
+      handleResize({ fabricRef })
+    })
+
+  }, [])
 
   return (
     <main className="h-screen border-red-200 flex flex-col overflow-hidden">
-      <Navbar 
+      <Navbar
         activeElement={activeElement}
         handleActiveElement={handleActiveElement}
       />
